1d01b586e3156c3fc92cbc5a98e6fe7abcd036a7
S-Mann <29407727+S-Mann@users.noreply.github.com>
2024-05-31 20:34:22 +0000

    ✨ My layout
    Signed-off-by: S-Mann <29407727+S-Mann@users.noreply.github.com>

--- keyboards/fingerpunch/sweeeeep/keymaps/default/keymap.c
+++ keyboards/fingerpunch/sweeeeep/keymaps/default/keymap.c
@@ -4,7 +4,6 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _QWERTY,
-    _COLEMAK,
     _LOWER,
     _RAISE,
     _ADJUST
@@ -38,46 +37,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               LOWER,        KC_ENT,       KC_QUOT,      KC_BSPC, KC_SPC,       RAISE
 ),
 
-/* Colemak
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   Q  |   W  |   F  |   P  |   B  |           |   J  |   L  |   U  |   Y  |   ;  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   A  |   R  |   S  |   T  |   G  |           |   M  |   N  |   E  |   I  |   O  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   Z  |   X  |   C  |   D  |   V  |           |   K  |   H  |   ,  |   .  |   /  |
- * `-------------+--------------------'           `----------------------------------'
- *                  ,--------------------.    ,--------------------.
- *                  | LOWER| Enter|   '  |    |BckSpc| Space| RAISE|
- *                  `--------------------'    `--------------------.
- */
-
-// Default config uses home row mods. So hold each of the keys on the home row to use ctrl, gui, alt, or shift
-[_COLEMAK] = LAYOUT_split_3x5_3(
-  KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,         KC_J,    KC_L,         KC_U,         KC_Y,         KC_SCLN,
-  LCTL_T(KC_A), LGUI_T(KC_R), LALT_T(KC_S), LSFT_T(KC_T), KC_G,         KC_M,    RSFT_T(KC_N), RALT_T(KC_E), RGUI_T(KC_I), RCTL_T(KC_O),
-  KC_Z,         KC_X,         KC_C,         KC_D,         KC_V,         KC_K,    KC_H,         KC_COMM,      KC_DOT,       KC_SLSH,
-                              LOWER,        KC_ENT,       KC_QUOT,      KC_BSPC, KC_SPC,       RAISE
-),
-
 /* Raise
  *
  * ,----------------------------------.           ,----------------------------------.
  * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Left | Down |  Up  | Right| Tab |           |      |   -  |   =  |   [  |   ]  |
+ * |  Left | Down |  Up  | Right| Tab |           |   `  |   -  |   =  |   [  |   ]  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Ctrl|   `  |  GUI |  Alt |      |           |      |      |      |   \  |   '  |
+ * |  Home | End  | PgUp | PgDw |     |           |      |      |      |   \  |   '  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,--------------------.
  *                  | LOWER|      |      |    |      |      | RAISE|
  *                  `--------------------'    `--------------------.
  */
 [_RAISE] = LAYOUT_split_3x5_3(
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,       _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS, KC_QUOT,
-                    _______, _______, _______,      _______, _______, _______
+  KC_1,    KC_2,    KC_3,       KC_4,           KC_5,       KC_6,       KC_7,      KC_8,    KC_9,       KC_0,
+  KC_LEFT, KC_DOWN, KC_UP,      KC_RGHT,        KC_TAB,     KC_GRV,     KC_MINS,   KC_EQL,  KC_LBRC,    KC_RBRC,
+  KC_HOME, KC_END,  KC_PAGE_UP, KC_PAGE_DOWN,   _______,    _______,    _______,   _______, KC_BSLS,    KC_QUOT,
+                    _______, _______, _______,  _______,    _______,    _______
 ),
 
 /* Lower
@@ -85,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |   (  |   )  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Esc |      |      |      |      |           |      |   _  |   +  |   {  |   }  |
+ * |  Esc |      |      |      |      |           |   ~  |   _  |   +  |   {  |   }  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Caps|   ~  |      |      |      |           |      |      |      |   |  |   "  |
+ * |  Caps|      |      |      |      |           |      |      |      |   |  |   "  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,--------------------.
  *                  | LOWER|      |      |    |  Del |      | RAISE|
@@ -95,29 +72,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_split_3x5_3(
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-  KC_ESC,  _______, _______, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,
-  KC_CAPS, KC_TILD, _______, _______, _______,      _______, _______, _______, KC_PIPE,  KC_DQT,
+  KC_ESC,  _______, _______, _______, _______,      KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,
+  KC_CAPS, _______, _______, _______, _______,      _______, _______, _______, KC_PIPE,  KC_DQT,
                     _______, _______, _______,      KC_DEL,  _______, _______
 ),
 
 /* Adjust (Lower + Raise)
  *
  * ,----------------------------------.           ,----------------------------------.
- * | RGB_T| RGB_R| RGB_F|      |QWERTY|           |   F1 |  F2  |  F3  |  F4  |  F5 |
+ * |      |      |      |      |QWERTY|           |   F1 |  F2  |  F3  |  F4  |  F5 |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * | SPD_I| HUE_I| SAT_I| VAL_I|COLEMK|           |   F6 |  F7  |  F8  |  F9  |  F10 |
+ * |      | Prev | Play | Next | Mute |           |   F6 |  F7  |  F8  |  F9  |  F10 |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * | SPD_D| HUE_D| SAT_D| VAL_D|      |           |  F11 |  F12 |      |      | Reset|
+ * | VolDw| VolUp|      |      |      |           |  F11 |  F12 |      |      | Reset|
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,--------------------.
  *                  | LOWER|      |      |    |      |      | RAISE|
  *                  `--------------------'    `--------------------.
  */
 [_ADJUST] =  LAYOUT_split_3x5_3(
-  RGB_TOG, RGB_RMOD, RGB_MOD, _______, TO(_QWERTY),       KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,
-  RGB_SPI, RGB_HUI,  RGB_SAI, RGB_VAI, TO(_COLEMAK),      KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,
-  RGB_SPD, RGB_HUD,  RGB_SAD, RGB_VAD, _______,           KC_F11,  KC_F12,  _______, _______,  QK_BOOT,
-                     _______, _______, _______,           _______, _______, _______
+  _______,              _______,                _______,                _______,                TO(_QWERTY),    KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,
+  _______,              KC_MEDIA_PREV_TRACK,    KC_MEDIA_PLAY_PAUSE,    KC_MEDIA_NEXT_TRACK,    KC_AUDIO_MUTE,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,
+  KC_AUDIO_VOL_DOWN,    KC_AUDIO_VOL_UP,        _______,                _______,                _______,        KC_F11,  KC_F12,  _______, _______,  QK_BOOT,
+                                                _______,                _______,                _______,        _______, _______, _______
 )
 };
 
